42335573cde20c1a04d7e0f9da24a6167049b28f
birme <jonas.birme@eyevinn.se>
2021-12-10 18:37:21 +0100

    fix: revert last change as timeout should be cleared in the finally block

--- output_plugins/mediapackage.ts
+++ output_plugins/mediapackage.ts
@@ -167,7 +167,6 @@ export class MediaPackageOutputDestination implements IOutputPluginDest {
             await timer(RETRY_DELAY);
           }
         } catch (err) {
-          clearTimeout(timeout);
           console.error(err);
           return false;
         } finally {
